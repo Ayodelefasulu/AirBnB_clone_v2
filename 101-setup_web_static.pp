8a497991ae4cfb06be2c2582b40628d8f6f3fbf4
Ayodelefasulu <fasuluayodele@gmail.com>
2024-04-04 22:06:42 -0700

    task 101

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,87 @@
+# 101-setup_web_static.pp
+
+# Ensure Nginx is installed
+package { 'nginx':
+  ensure => installed,
+}
+
+# Create required directories
+file { '/data':
+  ensure => directory,
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+file { '/data/web_static':
+  ensure => directory,
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+file { '/data/web_static/releases':
+  ensure => directory,
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+file { '/data/web_static/shared':
+  ensure => directory,
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+file { '/data/web_static/releases/test':
+  ensure => directory,
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+# Create a fake HTML file
+file { '/data/web_static/releases/test/index.html':
+  ensure  => file,
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  content => '<html><head></head><body>Holberton School</body></html>',
+}
+
+# Create symbolic link
+file { '/data/web_static/current':
+  ensure => link,
+  target => '/data/web_static/releases/test',
+  force  => true,
+  owner  => 'root',
+  group  => 'root',
+}
+
+# Update Nginx configuration
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => "\
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+
+    index index.html index.htm index.nginx-debian.html;
+
+    server_name _;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+    }
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+}",
+  notify => Service['nginx'],
+}
+
+# Restart Nginx service when configuration changes
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => File['/etc/nginx/sites-available/default'],
+}
+

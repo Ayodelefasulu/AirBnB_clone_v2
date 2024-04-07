b6816b73cabd92b610511e7a91433d0d28ea6629
Ayodelefasulu <fasuluayodele@gmail.com>
2024-04-06 19:21:13 -0700

    task 1 correction

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,30 +5,13 @@ from fabric.api import local
 from datetime import datetime
 
 def do_pack():
-    """Generates a .tgz archive from the web_static folder
-
-    Returns:
-        str: Archive path if successful, None otherwise
-    """
+    """generates a tgz archive"""
     try:
-        # Create the versions directory if it doesn't exist
-        local("mkdir -p versions")
-
-        # Get the current date and time
-        now = datetime.now()
-        timestamp = now.strftime("%Y%m%d%H%M%S")
-
-        # Create the archive filename with the timestamp
-        archive_name = "versions/web_static_{}.tgz".format(timestamp)
-
-        # Create the .tgz archive from the web_static folder
-        local("tar -cvzf {} web_static".format(archive_name))
-
-        # Return the archive path if successful
-        return archive_name
-    except Exception as e:
-        print("Error packing web_static:", e)
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except:
         return None
-
-if __name__ == "__main__":
-    do_pack()

0d12e3e6d72cc66e132a97cbcd54042a0202bc6d
Ayodelefasulu <fasuluayodele@gmail.com>
2024-04-24 09:18:27 -0700

    task 7

--- models/state.py
+++ models/state.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
 from models.base_model import BaseModel
+from models import storage
+from models.city import City
 
 
 class State(BaseModel):
     """ State class """
     name = ""
+
+    @property
+    def cities(self):
+        """ Getter method to return the list of City objects linked to the current State """
+        cities_list = []
+        all_cities = storage.all(City)
+        for city in all_cities.values():
+            if city.state_id == self.id:
+                cities_list.append(city)
+        return cities_list
